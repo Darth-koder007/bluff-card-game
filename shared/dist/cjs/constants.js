714672169512a59640fa1e978832d9796ed32ca4
Darth-koder007 <vj2010sn@gmail.com>
2025-09-14 21:25:11 +0200

    chore: cleanup

--- shared/dist/cjs/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RANKS = exports.SUITS = void 0;
-exports.SUITS = ['CLUBS', 'DIAMONDS', 'HEARTS', 'SPADES'];
-exports.RANKS = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
--- shared/dist/cjs/events.d.ts
+++
@@ -1,8 +0,0 @@
-import { GameState, Move } from "./types";
-export interface ServerToClientEvents {
-    gameState: (gameState: GameState) => void;
-}
-export interface ClientToServerEvents {
-    move: (move: Move) => void;
-    joinRoom: (room: string) => void;
-}
